f68e7bb4c43bf8c8691c916adaf9af141580ad54
jack eichner <jack8191@gmail.com>
2020-01-10 15:31:47 -0800

    Updates

---
+++ precache-manifest.7fc89b748ffe8865a3fc4f47483b4ca2.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5095593694a87d74b0c6bb87a30e8c58",
+    "url": "/stocktwits-search/index.html"
+  },
+  {
+    "revision": "e6dbab7b3de1aebd7ef2",
+    "url": "/stocktwits-search/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "e6dbab7b3de1aebd7ef2",
+    "url": "/stocktwits-search/static/js/2.b2249d5b.chunk.js"
+  },
+  {
+    "revision": "f032203ca460334c00de541c30a6078a",
+    "url": "/stocktwits-search/static/js/2.b2249d5b.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0ba8c1e05b1ee8346e00",
+    "url": "/stocktwits-search/static/js/main.6c58116d.chunk.js"
+  },
+  {
+    "revision": "dd1d2ebb27c301e90880",
+    "url": "/stocktwits-search/static/js/runtime-main.fe69337c.js"
+  }
+]);
